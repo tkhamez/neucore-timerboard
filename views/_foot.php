05e83981a3705762f62f010253e4c6eae21a6e12
Tian Khamez <tkhamez@gmail.com>
2020-05-22 17:08:09 +0200

    fix IT channel name

--- views/_foot.php
+++ views/_foot.php
@@ -5,7 +5,7 @@
     <footer class="navbar navbar-dark bg-brave shadow-1">
         <div class="align-self-center">
             Brave Collective Services. For support write to support@bravecollective.freshdesk.com
-            or ask in the ingame channel "Brave IT".
+            or ask in the ingame channel "Brave IT Team".
         </div>
     </footer>
 
